0a8992adab9011860975c4d171b9ddfa1298980c
punya <aquaeasy786@gmail.com>
2025-12-24 19:14:47 +0530

    Meta Description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,11 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-        <title>RentMyBook</title>
-        <meta name="description" content="It provides variety of books on rent on the basis of subscriptions." />
-        <link rel="icon" href="/rentmybooklogo.png"/>
+          <title>Rent Books Online in India @ ₹3/Day | RentMyBooks</title>
+          <meta name="description" content="Rent books online in India at just ₹3 per day. Affordable book rental for students with fast delivery and wide book collection. Save money with RentMyBooks." />
+          <link rel="icon" href="/rentmybooklogo.png" />
+          <meta name="keywords" content="rent books online, book rental india, rent textbooks, rent books for students, online book rental, cheap book rent, rent books india, textbook rental india, rent academic books
+"/>
         </Head>
         <body>
           <Main />
